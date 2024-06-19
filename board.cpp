e4a069ff66c98f163555abd7303af7dadc374ac8
oezgeb <oezgebayram@gmail.com>
2024-06-19 13:18:57 +0200

    initial commit Board

---
+++ board.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "board.hpp"
+
+std::string Board::place(int column, char player)
+{
+    int y=Board::m_y;
+    while(y>=0)
+    {
+        if(Board::field[y][column] == 'X')
+        {
+            Board::field[y][column] = player;
+            return "valid move";
+        }
+        else
+        {
+            y--;
+        }
+    }
+    return  "invalid move";
+}
+void Board::display()
+{
+    for(int i=0;i< Board::m_y;i++){
+        for (int j=0;j< Board::m_x;j++){
+            std::cout<< field[i][j];}
+            std::cout << std::endl;
+
+}
+}
+bool Board::check_winner(char player)
+{
+    for(int i=0;i< Board::m_y;i++){
+        for (int j=0;j< Board::m_x;j++){
+
+            if(field[i][j] == player)
+            {
+               int k=0;
+               while(k<4)
+               {
+                if(field[i+k][j] == player)  //Horizontal
+                {
+                    k=k+1;
+                }
+                else if(field[i][j+k]==player) //Vertikal
+                {
+                    k=k+1;
+                }
+                else if(field[i+k][j+k]==player) //Diagonal rechts
+                {
+                    k=k+1;
+                }
+                 else if(field[i-k][j+k]==player) //Diagonal links
+                {
+                    k=k+1;
+                }
+
+                else //Kein Sieg
+                {
+                    return 0;
+                }
+               }
+               return 1;
+            }
+
+        }
+    }
+}
+
+int Board::getsize()
+{
+   return Board::m_x, Board::m_y; //Gibt die Anzahl der Säulen zurück + Höhe des Boards
+
+}
---
+++ board.hpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#ifndef BOARD_HPP_INCLUDED
+#define BOARD_HPP_INCLUDED
+
+class Board
+{
+public:
+    Board(int x, int y)
+        : m_y(y)
+        , m_x(x)
+    {
+        for (int i = 0; i < m_y; i++)
+        {
+            for (int j = 0; j < m_x; j++)
+            {
+                field[i][j] = 'X';
+            }
+        }
+    }
+    std::string place(int column, char player);
+    void display();
+    bool check_winner(char player);
+    int getsize();
+
+private:
+    int m_x;
+    int m_y;
+    char field[100][100];
+};
+
+#endif // BOARD_HPP_INCLUDED
